a8459a3087e59a4041c6cdbf76696d171352ed4b
Romain Avonde <romain.avonde@capgemini.com>
2019-03-14 17:02:28 +0100

    Completed the module

--- WishList/Models/ApplicationUser.cs
+++ WishList/Models/ApplicationUser.cs
@@ -2,11 +2,18 @@
 {
     #region Usings
 
+    using System.Collections.Generic;
+
     using Microsoft.AspNetCore.Identity;
 
     #endregion
 
     public class ApplicationUser : IdentityUser
     {
+        #region Propriétés et indexeurs
+
+        public virtual ICollection<Item> Items { get; set; }
+
+        #endregion
     }
 }
