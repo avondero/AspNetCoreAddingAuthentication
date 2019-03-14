01a637be3517046cf5369914a1cda5c5df470200
Romain Avonde <romain.avonde@capgemini.com>
2019-03-14 15:25:21 +0100

    Completed the module

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿namespace WishList.Controllers
+{
+    #region Usings
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    using WishList.Models;
+
+    #endregion
+
+    [Authorize]
+    public class AccountController : Controller
+    {
+        #region Champs
+
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        #endregion
+
+        #region Constructeurs et destructeurs
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        #endregion
+    }
+}
